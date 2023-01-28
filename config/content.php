a658d1a3f5eb2301639ca5af7623b0c7383c62c1
Haris Hariyanto <harishariyanto313@gmail.com>
2023-01-28 23:58:44 +0700

    x

--- config/content.php
+++ config/content.php
@@ -5,7 +5,7 @@ return [
     'item_per_page' => 24,
     'related_questions' => 20,
 
-    'sitemap_items' => 80,
-    'sitemap_changefreq' => 'daily',
-    'sitemap_priority' => '0.8',
+    'sitemap_items' => 3000,
+    'sitemap_changefreq' => '',
+    'sitemap_priority' => '',
 ];
